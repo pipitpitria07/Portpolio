f3bf52b501917c886a7a425ef0d869f79eb61e4e
pipitpitria07 <pipitpitriapipitpitria08@gmail.com>
2023-04-12 00:40:24 +0700

    menambahkan file

---
+++ iPortfolio/about.php
@@ -0,0 +1,31 @@
+<section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>I am Pipit Pitria, a student at STMIK Bandung, Informatics Engineering Study Program, Semester Six, 
+            Participating in the Merdeka Campus Program, Independent Setudi Independent Batch, Four Partners,
+            PT NCI NF Computer, FullStack Web Developer Academy 
+          </p>
+        </div>
+
+        <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>7 jan 2001</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+62 821 2885 5812</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Majalengka, IDN</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>23</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>student</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>pitriapipi663@gmail.com</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
---
+++ iPortfolio/home.php
@@ -0,0 +1,27 @@
+<header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/photo.png" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.php?hal=home">Pipit Pitria</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="#" class="github"><i class="bx bxl-github"></i></a>
+          <a href="#" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active" href="index.php?hal=home"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#about" class="nav-link scrollto" href="index.php?hal=about"><i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#resume" class="nav-link scrollto" href="index.php?hal=resume"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+          <li><a href="#portfolio" class="nav-link scrollto" href="index.php?hal=portfolio"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+          <li><a href="#services" class="nav-link scrollto" href="index.php?hal=services"><i class="bx bx-server"></i> <span>Services</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto" href="index.php?hal=contact"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav>
+    </div>
+</header>
